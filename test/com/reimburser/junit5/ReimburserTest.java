4a3d9e081b21a76a63fa8f4e5b4d077890813b30
Dean <dean@deankuhta.com>
2018-10-10 20:16:19 -0400

    added unit tests for corner cases

--- test/com/reimburser/junit5/ReimburserTest.java
+++ test/com/reimburser/junit5/ReimburserTest.java
@@ -2,6 +2,7 @@ package com.reimburser.junit5;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import org.junit.jupiter.api.Test;
 import main.java.Reimburser;
 
@@ -15,6 +16,25 @@ class ReimburserTest {
 		LocalDate endDate = LocalDate.parse("2015-09-03");
 		int result = r.getDateRange(startDate, endDate);
 		assertEquals(3, result);		
+	}
+	
+	@Test
+	void getDateRange_nullpointer() {
+		LocalDate startDate = null;
+		LocalDate endDate = LocalDate.parse("2015-09-03");
+		assertThrows(NullPointerException.class,
+				() -> {
+					r.getDateRange(startDate, endDate);
+				});
+	}	
+	
+	@Test
+	void getDateRange_parse_error() {
+		String date = "20154-09-03";
+		assertThrows(DateTimeParseException.class,
+				() -> {
+					LocalDate.parse(date);
+				});
 	}
 	
 	@Test
